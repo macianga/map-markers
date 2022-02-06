67612c565f4bf0019a393b9040c311d8450cc72b
maciek <macieq2048@gmail.com>
2022-02-06 20:39:43 +0100

    refactor API

--- api/endpoints/user.py
+++ api/endpoints/user.py
@@ -1,13 +1,13 @@
 import httpx as httpx
-from fastapi import APIRouter, Body
+from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
 
 from api.config import RANDOM_USER_SERVICE_URL
-from api.database.database import retrieve_users, retrieve_user, add_user, delete_user
+from api.database.database import retrieve_users, add_user, delete_user
 from api.models.users import ResponseModel, ErrorResponseModel, UserModel, Coordinates
 
 router = APIRouter(
-    prefix="/user",
+    prefix="/users",
     tags=["users"],
 )
 
@@ -15,62 +15,34 @@ router = APIRouter(
 @router.get("/", response_description="Users retrieved")
 async def get_users():
     users = await retrieve_users()
-    return ResponseModel(users, "Users data retrieved successfully") \
-        if len(users) > 0 \
-        else ResponseModel(
-        users, "Empty list returned")
+    return ResponseModel(users, "Users data retrieved successfully")
+
+
+@router.delete("/", response_description="User data deleted from the database")
+async def delete_user_data(user_id: str):
+    deleted_user = await delete_user(user_id)
+    if deleted_user:
+        return ResponseModel(f"User with ID: {user_id} removed", "User deleted successfully")
+    return ErrorResponseModel("An error occurred", 404, f"User with id {user_id} doesn't exist")
 
 
 @router.get("/create", response_description="Create user")
 async def create_user():
     async with httpx.AsyncClient() as client:
         response = await client.get(RANDOM_USER_SERVICE_URL)
+        if response.status_code != 200:
+            return ErrorResponseModel("An error occurred", 404, f"Can't create user.")
         json = response.json()
         user_json_data = json['results'][0]
 
-        print(user_json_data)
-
-        user_location = Coordinates(
-            lat=float(user_json_data["location"]["coordinates"]["latitude"]),
-            lng=float(user_json_data["location"]["coordinates"]["longitude"]),
-        )
         user = UserModel(email=user_json_data["email"],
                          firstname=user_json_data["name"]["first"],
                          lastname=user_json_data["name"]["last"],
-                         coordinates=user_location
+                         coordinates=Coordinates(
+                             lat=float(user_json_data["location"]["coordinates"]["latitude"]),
+                             lng=float(user_json_data["location"]["coordinates"]["longitude"]),
+                         )
                          )
 
         new_user = await add_user(jsonable_encoder(user))
         return ResponseModel(new_user, "User added successfully.")
-
-
-@router.get("/{id}", response_description="User data retrieved")
-async def get_user_data(user_id):
-    user = await retrieve_user(user_id)
-    return ResponseModel(user, "User data retrieved successfully") \
-        if user \
-        else ErrorResponseModel("An error occurred.", 404, "Users doesn't exist.")
-
-
-@router.post("/", response_description="User data added into the database")
-async def add_user_data(user: UserModel = Body(...)):
-    user = jsonable_encoder(user)
-    new_user = await add_user(user)
-    return ResponseModel(new_user, "User added successfully.")
-
-
-@router.delete("/{user_id}", response_description="User data deleted from the database")
-async def delete_user_data(user_id: str):
-    print(user_id)
-    deleted_user = await delete_user(user_id)
-    return ResponseModel("User with ID: {} removed".format(user_id), "User deleted successfully") \
-        if deleted_user \
-        else ErrorResponseModel("An error occurred", 404, "User with id {0} doesn't exist".format(id))
-
-# @router.put("{id}")
-# async def update_user(id: str, req: UpdateStudentModel = Body(...)):
-#     updated_student = await update_user_data(id, req.dict())
-#     return ResponseModel("Student with ID: {} name update is successful".format(id),
-#                          "Student name updated successfully") \
-#         if updated_student \
-#         else ErrorResponseModel("An error occurred", 404, "There was an error updating the student.".format(id))
