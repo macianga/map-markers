c6e4655c51b5199cbefb2d0c3fbbab77963c08e9
maciek <macieq2048@gmail.com>
2022-02-06 21:02:42 +0100

    remove unused functions & refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-/* global google */
 import "./App.css";
 
-import * as React from "react";
 import {useEffect, useState} from "react";
 import {Status, Wrapper} from "@googlemaps/react-wrapper";
 import CustomMap from "./components/CustomMap";
 import Marker, {CustomMarkerType} from "./components/Marker";
 import {createUser, deleteUser, getUsers, UserType} from "./utils/apiHelpers";
 import {getUserWithId} from "./utils/utils";
-import Animation = google.maps.Animation;
 
 
 const render = (status: Status) => {
@@ -16,51 +13,39 @@ const render = (status: Status) => {
 };
 
 function App() {
-  const [clicks, setClicks] = React.useState<google.maps.LatLng[]>([]);
   const [selectedUser, setSelectedUser] = useState<UserType | null>(null);
-  const [zoom, setZoom] = React.useState(2); // initial zoom
-  const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
+  const [zoom, setZoom] = useState(2); // initial zoom
+  const [center, setCenter] = useState<google.maps.LatLngLiteral>({
     lat: 0,
     lng: 0,
   });
-
   const [users, setUsers] = useState<Array<UserType>>([]);
 
-  const onClick = (e: google.maps.MapMouseEvent) => {
-    // avoid directly mutating state
-    setClicks([...clicks, e.latLng!]);
-  };
-
-  const fetchAndSetUsers = async () =>{
+  const fetchAndSetUsers = async () => {
     const [responseOk, responseData] = await getUsers();
-    if(responseOk){
+    if (responseOk) {
       setUsers(responseData.response);
     }
   }
 
   const createNewUser = async () => {
     const [responseOk, responseData] = await createUser();
-    if(responseOk){
+    if (responseOk) {
       await fetchAndSetUsers();
     }
   }
 
   const deleteUserFunc = async () => {
-    if(!selectedUser) return;
+    if (!selectedUser) return;
     await deleteUser(selectedUser.id);
     await fetchAndSetUsers()
     setSelectedUser(null)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchAndSetUsers();
   }, [])
 
-  const onIdle = (m: google.maps.Map) => {
-    console.log("onIdle");
-    setZoom(m.getZoom()!);
-    setCenter(m.getCenter()!.toJSON());
-  };
 
   const onMarkerClick = (marker: CustomMarkerType) => {
     const user = getUserWithId(marker.id, users);
@@ -73,24 +58,22 @@ function App() {
       <Wrapper apiKey={"AIzaSyDfeRyZG3CgJ6145NeXjPrpC7TbxQvc54I"} render={render}>
         <CustomMap
           center={center}
-          onClick={onClick}
-          onIdle={onIdle}
           zoom={zoom}
-            style={{flexGrow: "1", height: "100%"}}
-          >
-            {
-              users.map((user, i) => (
-                <Marker
-                  key={i}
-                  position={user.coordinates}
-                  onClick={onMarkerClick}
-                  id={user.id}
-                  // animation={user.id === selectedUser?.id ? Animation.BOUNCE : null}
-                />
-              ))
-            }
-          </CustomMap>
-        </Wrapper>
+          style={{flexGrow: "1", height: "100%"}}
+        >
+          {
+            users.map((user, i) => (
+              <Marker
+                key={i}
+                position={user.coordinates}
+                onClick={onMarkerClick}
+                id={user.id}
+                // animation={user.id === selectedUser?.id ? Animation.BOUNCE : null}
+              />
+            ))
+          }
+        </CustomMap>
+      </Wrapper>
       <div className="bg-gray-600 w-4/12">
         <h1 className="text-5xl text-center mt-2 font-bold mb-10 text-cyan-600">Users on map</h1>
         <div className="flex items-center justify-center">
@@ -104,15 +87,15 @@ function App() {
         {selectedUser &&
             <div className="flex flex-col items-center justify-center mt-3">
                 <hr className="border-blue-500 border-2 w-[90%] mb-2"/>
-              <p>Name: {selectedUser.firstname}</p>
-              <p>Surname: {selectedUser.lastname}</p>
-              <p>Email: {selectedUser.email}</p>
-              <button
-                className="p-2 pl-5 pr-5 border-2 border-danger rounded-md text-danger w-fit
-                   hover:border-danger-hover hover:text-danger-hover transition-all font-bold text-2xl mt-3"
-                onClick={deleteUserFunc}
-              >DELETE USER
-              </button>
+                <p>Name: {selectedUser.firstname}</p>
+                <p>Surname: {selectedUser.lastname}</p>
+                <p>Email: {selectedUser.email}</p>
+                <button
+                    className="p-2 pl-5 pr-5 border-2 border-danger rounded-md text-danger w-fit
+                 hover:border-danger-hover hover:text-danger-hover transition-all font-bold text-2xl mt-3"
+                    onClick={deleteUserFunc}
+                >DELETE USER
+                </button>
             </div>
         }
       </div>
