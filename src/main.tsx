2e9b2b1787061808ce574df5f910260aba4f38e1
maciek <macieq2048@gmail.com>
2022-02-07 18:52:03 +0100

    change page title based on environment

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-document.title = import.meta.env.VITE_PAGE_TITLE
+document.title = import.meta.env.VITE_PAGE_TITLE || "Map app"
 
 ReactDOM.render(
   <React.StrictMode>
